f4eaac2358f750f399754479fdd2c20ffb3ea616
Almortagel <115498589+Almortagel12@users.noreply.github.com>
2023-04-12 22:55:54 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ class Config:
     API_HASH = "a5e14021456afd496e7377331e2e5bcf"
     TOKEN = "5977145501:AAGoAjqHVaIHbu2ocYKlPI5cAfVGd9fXoiA"  
     START_PIC = "https://telegra.ph//file/971c9cf892b195cd7c679.jpg"
-    CHAT = "-1001622606659"
+    CHAT = "ALMORTAGELTECH"
