2ad2842d7f640cc04621068603751ef6a505bc93
Almortagel <115498589+Almortagel12@users.noreply.github.com>
2023-04-13 00:55:26 +0200

    Update __init__.py

--- HackSessionBot/__init__.py
+++ HackSessionBot/__init__.py
@@ -1,22 +1,17 @@
+
 import os
 import asyncio
 import logging
-from config import Config
+from config import *
 from pyrogram import Client
 from rich.console import Console
 from rich.table import Table
-from HackSessionBot.Helpers.data import LOG_TEXT,ART
+from Romeo.Helpers.data import LOG_TEXT
 from pyromod import listen 
-
-#getting variables
-API_ID = Config.API_ID
-API_HASH = Config.API_HASH
-TOKEN = Config.TOKEN
-START_PIC = Config.START_PIC
 
 
 if not START_PIC:
-    START_PIC = "https://graph.org/file/864483e9fb1cec38b67fe.jpg"
+    START_PIC = "https://telegra.ph//file/a9a90059ae47fa840b0b3.jpg"
 
 #rich
 LOG = Console()
@@ -26,34 +21,23 @@ logging.basicConfig(level=logging.INFO)
 
 #client
 app = Client(
-    "SupremeStark",
+    "romeorj",
     api_id = API_ID,
     api_hash = API_HASH,
-    bot_token = TOKEN )
+    bot_token = BOT_TOKEN )
     
 
 
-async def HackSessionBot():
+async def Romeo():
     os.system("clear")
     header = Table(show_header=True, header_style="bold green")
     header.add_column(LOG_TEXT)
     LOG.print(header)
-    LOG.print(f"[bold cyan]{ART}")
+    LOG.print(f"[bold cyan]𝐂𝐑𝐄𝐀𝐓𝐎𝐑 ❥︎𝕾🅡︎🅐︎🅙︎𝕾...")
     LOG.print("[bold yellow]sᴛᴀʀᴛɪɴɢ ʏᴏᴜʀ ʙᴏᴛ ɴᴏᴡ.......")
     await app.start()    
     
 
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(HackSessionBot())    
-
-
-
-    
-    
-
-    
-    
-
-
-
+loop.run_until_complete(HackSessionBot())
