a570a9fbc4e70945dd3972870b1b61ffb881ed09
Almortagel <115498589+Almortagel12@users.noreply.github.com>
2023-04-12 22:44:04 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-class config:
+class Config:
     API_ID = "14911221"
     API_HASH = "a5e14021456afd496e7377331e2e5bcf"
-    TOKEN = "5977145501:AAF1e-3f85aoeXgZ3W3ToVkUJACUw-9NHI8"  
+    TOKEN = "5977145501:AAGoAjqHVaIHbu2ocYKlPI5cAfVGd9fXoiA"  
     START_PIC = "https://telegra.ph//file/971c9cf892b195cd7c679.jpg"
+    CHAT = "-1001622606659"
