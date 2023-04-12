776ec510e80f561d5f808a72193de233ccd92f95
Almortagel <105941198+Almortage@users.noreply.github.com>
2023-04-12 17:32:04 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     API_ID = "14911221"
     API_HASH = "a5e14021456afd496e7377331e2e5bcf"
-    TOKEN = "5977145501:AAGYlUZ6KZIIheLhSQh0h5cyl7cqA-3iyoQ"  
+    TOKEN = "5977145501:AAGoAjqHVaIHbu2ocYKlPI5cAfVGd9fXoiA"  
     START_PIC = "https://telegra.ph//file/971c9cf892b195cd7c679.jpg"
